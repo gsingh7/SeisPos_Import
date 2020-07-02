09bf3ac3c3315320739ae57e374af391a94ca301
Gurpreet Singh <gurpreet.s.singh@gmail.com>
2020-07-02 12:14:09 +0100

    Initial code push

---
+++ QGIS_env_set.bat
@@ -0,0 +1,7 @@
+@echo off
+call "C:\Program Files\QGIS 3.8\bin\o4w_env.bat"
+call "C:\Program Files\QGIS 3.8\bin\qt5_env.bat"
+call "C:\Program Files\QGIS 3.8\bin\py3_env.bat"
+set PATH=%PATH%;C:\Users\gsingh\AppData\Roaming\Python\Python37\Scripts\
+@echo on
+cmd
